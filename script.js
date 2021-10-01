040051289c58d3344d5edc6a9b63e3a831ad4cab
Guilherme <guilhermenied01@gmail.com>
2021-10-01 12:02:10 -0300

    a

---
+++ script.js
@@ -0,0 +1,11 @@
+function Enviar() {
+    const email = document.getElementById('email')
+
+    if (email.value == "") {
+        window.alert('Capo de email vazio')
+        
+    }else{
+        window.alert('Obriga por cadastrar seu email')
+    }
+    
+}
